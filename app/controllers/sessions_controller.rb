1fc592ad594e86f25e86c47f7fe7ab7a2260cbd8
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-09 21:01:51 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+  # look up a user in the database, verify their login credentials, and then store the authenticated user's id in the session.
   def create
   end
 end
