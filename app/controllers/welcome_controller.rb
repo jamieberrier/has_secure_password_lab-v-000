7e956d924ada4ecb760370fe77829766f3362bef
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-09 21:48:50 +0000

    Automatically backed up by Learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   before_action :require_logged_in
   def show
-    @user = User.find(session[:user_id])
+    #@user = User.find(session[:user_id])
   end
 end
