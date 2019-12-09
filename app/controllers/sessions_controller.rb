c13d4212db92a57eede0f53497fd4c5fedd22cef
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-09 21:05:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   # look up a user in the database, verify their login credentials,
   # and then store the authenticated user's id in the session.
   def create
-    binding.pry
     @user = User.find_by(name: params[:user][:name])
     session[:user_id] = @user.id
   end
