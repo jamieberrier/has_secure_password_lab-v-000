20039291b1ca51dfa679c12dfa88483e25af7a0e
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-09 21:52:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @user = User.find_by(id: session[:user_id]) || User.new
+    @user =|| User.find_by(id: session[:user_id])
   end
 
   def logged_in?
