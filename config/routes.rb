8aa65979a1beba8ed2b90bac2ea545bf1d78fc4c
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-09 21:37:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
+  get "/signup" => "users#new"
+  post "/signup" => "users#create"
+
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
 
