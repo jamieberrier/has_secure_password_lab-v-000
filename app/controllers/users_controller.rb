1e459ae177e0da425bbdf9630102d030703e6a38
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 01:47:56 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    return redirect_to signup_path unless @user.save
+    session[:user_id] = @user.id
+    redirect_to root_path
+=begin
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
       redirect_to signup_path
     end
+=end
   end
 
   private
