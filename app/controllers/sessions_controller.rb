cd0b3dd4fcb32a0a2e8e43c975162ff3de8ecfe5
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 01:00:00 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     authenticated = @user.try(:authenticate, params[:user][:password])
     # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
     flash[:error] = "Invalid Log In Credential(s)"
-    
+
     return redirect_to login_path unless authenticated
     
     session[:user_id] = @user.id
