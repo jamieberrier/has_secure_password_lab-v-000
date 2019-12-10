2512bfd3827b276400b7cf53666b5e5856b980e5
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 01:19:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   # and then store the authenticated user's id in the session.
   def create
     @user = User.find_by(name: params[:user][:name])
+    binding.pry
     authenticated = @user.try(:authenticate, params[:user][:password])
     # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
     flash[:error] = "Invalid Log In Credential(s)"
