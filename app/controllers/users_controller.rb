a96faf5d88b54cad472d9ccb9cad7c417491404c
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 01:49:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     return redirect_to signup_path unless @user.save
+    
     session[:user_id] = @user.id
     redirect_to root_path
   end
