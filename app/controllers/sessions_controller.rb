c04410fca9dc9c22b4b413c1f15de4755e8dc14a
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 00:37:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
     # return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    # try
+    # @user.try(:authenticate, params[:user][:password])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
