cba31f58f5bf6fa3d8168fcfa924db0b0ec31b84
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-10 00:50:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     authenticated = @user.try(:authenticate, params[:user][:password])
     # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
+    return redirect_to login_path unless authenticated
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
